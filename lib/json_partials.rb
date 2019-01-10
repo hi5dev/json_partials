219c7023392e88e470704776f413c9b2e13aa1d9
Travis Haynes <travis@hi5dev.com>
2019-01-10 15:35:13 -0800

    initial

---
+++ lib/json_partials.rb
@@ -0,0 +1,6 @@
+require "json_partials/version"
+
+module JsonPartials
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/json_partials/version.rb
@@ -0,0 +1,3 @@
+module JsonPartials
+  VERSION = "0.1.0"
+end
